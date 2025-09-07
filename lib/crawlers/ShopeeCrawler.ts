d5e1800d1d2797b9631e988fda210b924d32ed3f
Rahman Nur Hadi <mmn6d6d6e@users.noreply.github.com>
2025-09-07 18:26:16 +0700

    Update ShopeeCrawler.ts

--- lib/crawlers/ShopeeCrawler.ts
+++ lib/crawlers/ShopeeCrawler.ts
@@ -32,8 +32,7 @@ export class ShopeeCrawler extends BaseCrawler {
       args: [
         '--disable-blink-features',
         '--disable-blink-features=AutomationControlled',
-        '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-        '--no-sandbox'
+        '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
       ]
     };
 
